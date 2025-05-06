ca8a975ca8d2be203409b8ba9723cee79d075c8f
Lenovo <parvr7869@gmail.com>
2025-05-06 22:16:42 +0530

    triangle

---
+++ src/area_triangle.java
@@ -0,0 +1,9 @@
+public class area_triangle {
+    public static void main(String[] args){
+        // find the area of triangle with base = 5.6 cm and the height =9.89cm
+        float base = 5.6f;
+        float height = 9.89f;
+        double area = (double) 1/2*base*height;
+        System.out.println("area of a tringle is = "+area);
+    }
+}
