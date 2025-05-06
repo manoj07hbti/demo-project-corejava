f1d1e8c3dfff4642f87b16a3d50db70a2cb00579
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-06 22:32:51 +0530

    Increment Operator

---
+++ src/testNeeshu/IncrementOperator.java
@@ -0,0 +1,12 @@
+    package testNeeshu;
+
+    public class IncrementOperator {
+
+        public static void main(String[] args) {
+            int d=34;
+            d++;
+            System.out.println("Increment Operator: "+d);
+        }
+    }
+
+
