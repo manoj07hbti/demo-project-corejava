ec91296ff5cdd25a32955cc04cd939b752bf3193
Lenovo <parvr7869@gmail.com>
2025-05-06 22:07:54 +0530

    testing feature

---
+++ src/area.java
@@ -0,0 +1,9 @@
+public class area {
+    public static void main(String[] args){
+        // find the area of a circle with radius of 7.59
+        float radius = 7.59f;
+        float pi = 3.14f;
+        float area = pi*radius*radius;
+        System.out.println("area of acircle is = "+area);
+    }
+}
