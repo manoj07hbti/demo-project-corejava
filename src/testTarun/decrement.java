2db3f6da8d73ac0f47464225a4b6cbbe38d6bb9a
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-05-07 19:16:00 +0530

    testing git

---
+++ src/testTarun/decrement.java
@@ -0,0 +1,14 @@
+package testTarun;
+
+public class decrement {
+
+    public static void main(String[] args) {
+        int a=101;
+        a--;
+
+        System.out.println("answer= "+a);
+
+    }
+
+
+}
---
+++ src/testTarun/subtraction.java
@@ -0,0 +1,16 @@
+package testTarun;
+
+public class subtraction {
+
+    public static void main(String[] args) {
+
+        int a=40;
+        int b=20;
+        int sum = a-b;
+
+        System.out.println("sum of all= "+sum);
+
+    }
+
+
+}
