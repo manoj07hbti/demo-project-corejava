3d0391940a450db8c437fea0346f0286cfda5751
NotBladezy <mrbladezy@gmail.com>
2025-05-08 19:09:33 +0530

    Git Testing

---
+++ src/testing_git/HelloGit.java
@@ -0,0 +1,11 @@
+package testing_git;
+
+public class HelloGit
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("Hello Git");
+        System.out.println("Hello GitHub");
+    }
+}
