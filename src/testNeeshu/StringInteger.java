8bb34161e2f570b9e652511330f3d87cb45f8c19
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-08 18:30:50 +0530

    Test

---
+++ src/testNeeshu/StringInteger.java
@@ -0,0 +1,40 @@
+package testNeeshu;
+
+public class StringInteger {
+
+    public static void main(String[] args) {
+        String str1 = "Neeshu";
+        String str2 = "Rahul";
+        String str3 = "Monu";
+        String str4 = "Anil";
+        String str5 = "Kavi";
+        String str6 = "Jenny";
+        String str7 = "Abhishek";
+        String str8 = "Sarvesh";
+        String str9 = "Mukesh";
+        String str10 = "Sanskar";
+
+        int int1 = 27;
+        int int2 = 24;
+        int int3 = 30;
+        int int4 = 43;
+        int int5 = 50;
+        int int6 = 16;
+        int int7 = 13;
+        int int8 = 38;
+        int int9 = 19;
+        int int10 = 6;
+
+        System.out.println("Name" + " : Age");
+        System.out.println(str1 + ": " + int1);
+        System.out.println(str2 + ": " + int2);
+        System.out.println(str3 + ": " + int3);
+        System.out.println(str4 + ": " + int4);
+        System.out.println(str5 + ": " + int5);
+        System.out.println(str6 + ": " + int6);
+        System.out.println(str7 + ": " + int7);
+        System.out.println(str8 + ": " + int8);
+        System.out.println(str9 + ": " + int9);
+        System.out.println(str10 + ": " + int10);
+    }
+}
