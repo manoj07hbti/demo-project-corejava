748b4b889a3d9168f9dacdd6a13c33a8564a040c
sunny  hans <sunnyhans8642@gmail.com>
2025-05-08 18:27:57 +0530

    mixed variable

---
+++ src/git_branch_practice/Mixed_variable.java
@@ -0,0 +1,22 @@
+package git_branch_practice;
+
+public class Mixed_variable {
+    public static void main(String[] args) {
+
+        int a=45;
+        int b=34;
+        int sum =a+b;
+        System.out.println("sum of a+b is:" +sum);
+
+        int subtraction =a-b;
+        System.out.println("subtraction of a&b is :" +subtraction);
+
+        a=22;
+        b=314;
+        int c=43;
+        double bodmas=(double)a+b/c;
+        System.out.println("bodmas :" +bodmas);
+
+
+    }
+}
