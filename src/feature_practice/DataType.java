634371e6b934119764d4c6023d39820816f52a7c
sunny  hans <sunnyhans8642@gmail.com>
2025-05-08 15:12:05 +0530

    added DataType

---
+++ src/feature_practice/DataType.java
@@ -0,0 +1,12 @@
+package feature_practice;
+
+public class DataType {
+    public static void main(String[] args) {
+        String name="rohit";
+        String country="germany";
+        int stay= 1;
+        System.out.println("my friend :"+ name +" stays in " + country + " from "+ stay +" year");
+
+
+    }
+}
