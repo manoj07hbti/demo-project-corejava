0aa1a7bdd09b7fe0b4d36193ce5ede886516885a
Lenovo <parvr7869@gmail.com>
2025-05-08 18:40:00 +0530

    variable

---
+++ src/aadhar.java
@@ -0,0 +1,12 @@
+
+public class aadhar {
+ public static void main(String[] args) {
+     long aadhar = 903148876487L;
+     String name = "parvej";
+     String gender = "male";
+     int age = 21;
+     System.out.println("aadar number = "+aadhar +"name = "+name +"gender  = "+gender +"age = "+age);
+
+
+ }
+}
--- src/area.java
+++ src/area.java
@@ -3,7 +3,7 @@ public class area {
         // find the area of a circle with radius of 7.59
         float radius = 7.59f;
         float pi = 3.14f;
-        float area = pi*radius*radius;
+        double area = (double)pi*radius**2;
         System.out.println("area of acircle is = "+area);
     }
 }
