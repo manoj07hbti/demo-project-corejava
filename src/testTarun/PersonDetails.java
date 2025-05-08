dd7789aa2c0027102e5851ed82651e00be7f803a
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-05-08 23:47:59 +0530

    person details

---
+++ src/testTarun/PersonDetails.java
@@ -0,0 +1,33 @@
+package testTarun;
+
+import javafx.beans.binding.DoubleExpression;
+
+public class PersonDetails {
+    public static void main(String[] args) {
+
+        String firstname = "tarun";
+        String lastname = "sachdeva";
+        int age = 21;
+        int  height = 183;
+        Double weight = 65.60;
+        long number = 1234567890;
+        String city = "delhi";
+        String country = "india";
+        String collage = "ignou";
+        String course = "BCA";
+        String bloodGroup = "B+";
+
+        System.out.println("firstname= "+firstname);
+        System.out.println("lastname= "+lastname);
+        System.out.println("age= "+age);
+        System.out.println("height= "+height);
+        System.out.println("weight= "+weight);
+        System.out.println("number= "+number);
+        System.out.println("city= "+city);
+        System.out.println("country= "+country);
+        System.out.println("collagename= "+collage);
+        System.out.println("course= "+course);
+        System.out.println("bloodgroup= "+bloodGroup);
+    }
+
+}
