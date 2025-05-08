6cc0a5d76f71e3282ccf7b0a3943034844cb63a3
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-08 23:40:02 +0530

    Remainder Code

---
+++ src/testNeeshu_/Remainder.java
@@ -0,0 +1,12 @@
+package testNeeshu_;
+
+public class Remainder {
+
+    // This is remainder code
+
+    public static void main(String[] args) {
+        int a= 1098;
+
+        System.out.println("Remainder: "+a%19);
+    }
+}
