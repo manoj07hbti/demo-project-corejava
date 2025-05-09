a661cfdd8a6db5eee2f0d71a51d30378d26c2078
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-05-09 19:48:20 +0530

    ClassTest

---
+++ src/testTarun/ClassTest.java
@@ -0,0 +1,62 @@
+package testTarun;
+
+public class ClassTest {
+
+    public static void main(String[] args) {
+// student no 1 :-
+
+        String student1 = "Ayush";
+        String subject = "English";
+        int rollno = 5;
+        int marks = 20;
+        int outof = 50;
+
+// student no 2 :-
+
+        String student2 = "Mayank";
+        String subject2 = "Maths";
+        int rollno2 = 10;
+        int marks2 = 23;
+        int outof2 = 50;
+
+// student no 3 :-
+
+        String student3 = "Rahul";
+        String subject3 = "Social Science";
+        int rollno3 = 15;
+        int marks3 = 38;
+        int outof3 = 50;
+
+// student no 4 :-
+
+        String student4 = "Mehak";
+        String subject4 = "Science";
+        int rollno4 = 15;
+        int marks4 = 29;
+        int outof4 = 50;
+
+// deatils of all student :-
+
+        System.out.println("student1= "+student1);
+        System.out.println("subject= "+subject);
+        System.out.println("rollno= "+rollno);
+        System.out.println("marks= "+marks+"/"+outof);
+
+        System.out.println("student2= "+student2);
+        System.out.println("subject2= "+subject2);
+        System.out.println("rollno2= "+rollno2);
+        System.out.println("marks2= "+marks2+"/"+outof2);
+
+        System.out.println("student3= "+student3);
+        System.out.println("subject3= "+subject3);
+        System.out.println("rollno3= "+rollno3);
+        System.out.println("marks3= "+marks3+"/"+outof3);
+
+        System.out.println("student4= "+student4);
+        System.out.println("subject4= "+subject4);
+        System.out.println("rollno4= "+rollno4);
+        System.out.println("marks4= "+marks4+"/"+outof4);
+
+    }
+
+}
