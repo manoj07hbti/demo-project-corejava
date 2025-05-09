b9ede82868d115e54739ce4100ad9c25125ee59d
HARISH KUMAR <harish.kumar388497@gmail.com>
2025-05-09 05:56:14 +0530

    Testing git feature

---
+++ src/variable/FeatureTest.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class FeatureTest {
+    public static void main(String[] args) {
+        System.out.println("This is my first commit ");
+        System.out.println("testing git feature testing");
+    }
+}
