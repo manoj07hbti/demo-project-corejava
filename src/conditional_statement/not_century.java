a8645a6447bab3308870864977ed67048d8e9536
Lenovo <parvr7869@gmail.com>
2025-05-10 18:06:08 +0530

    assignment

---
+++ src/conditional_statement/not_century.java
@@ -0,0 +1,9 @@
+package conditional_statement;
+
+public class not_century {
+    public static void main(String[]args){
+        int runs = 100;
+        if(runs<100)
+            System.out.println("not a century " +runs);
+    }
+}
