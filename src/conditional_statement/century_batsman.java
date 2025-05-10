e87aed3bf9d596cbacfcde44ef08a3c5f705abaa
Lenovo <parvr7869@gmail.com>
2025-05-10 17:56:41 +0530

    assignment

---
+++ src/conditional_statement/century_batsman.java
@@ -0,0 +1,10 @@
+package conditional_statement;
+
+public class century_batsman {
+    public static void main(String[] args){
+        //write a code to check century of a batsman
+        int runs = 100;
+        if(runs >= 100)
+            System.out.println("check century of a batsman >= "+runs);
+    }
+}
