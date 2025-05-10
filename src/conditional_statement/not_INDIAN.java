f47357eeaf489118f4f24aef95fff9bd0f99ba64
Lenovo <parvr7869@gmail.com>
2025-05-10 18:34:21 +0530

    not INDIAN

--- src/conditional_statement/not_INDIAN.java
+++ src/conditional_statement/not_INDIAN.java
@@ -1,4 +1,10 @@
 package conditional_statement;
 
 public class not_INDIAN {
+    public static void main(String[] args){
+        //if candidate is not from INDIA then print NON INDIAN
+        String citizenship = "INDIA";
+        if(citizenship != "INDIA" )
+            System.out.println("citizenship is not INDIAN");
+    }
 }
