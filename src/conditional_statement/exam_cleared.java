8f84498e13de96175c93aa03c305e4f284d8e690
Lenovo <parvr7869@gmail.com>
2025-05-10 18:15:59 +0530

    assignment

---
+++ src/conditional_statement/exam_cleared.java
@@ -0,0 +1,11 @@
+package conditional_statement;
+
+public class exam_cleared {
+    public static void main(String[] args){
+        //if candidate has marks greater than 33 then it will print cleared the exam.
+        int marks = 34;
+        if(marks>33)
+            System.out.println("candidate cleared the exam "+marks);
+
+    }
+}
