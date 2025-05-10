f3198f6a5b9dc523cbc5c3ffda263a29fbd04a85
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-10 17:54:44 +0530

    CenturyEligible Code

--- src/conditional_statement/CenturyEligible.java
+++ src/conditional_statement/CenturyEligible.java
@@ -5,10 +5,12 @@ public class CenturyEligible {
     public static void main(String[] args) {
 
         int score=99;
-
+//check century of a batsman
         if (score==100){
             System.out.println("Century of a Batsman..: "+score);
         }
+
+        //check not century of a batsman
         if (score!=100){
             System.out.println("Century is not of a Batsman..: "+score);
         }
