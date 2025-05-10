88a5da5ea8aa0971d7db7b1e2f27d3e424f78549
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-10 16:24:49 +0530

    Added VolumeCuboid Code

---
+++ src/testNeeshu_/VolumeCuboid.java
@@ -0,0 +1,14 @@
+package testNeeshu_;
+
+public class VolumeCuboid {
+
+    public static void main(String[] args) {
+        int length=12;
+        int width=17;
+        double height=19.8;
+
+        double volume=length*width*height;
+
+        System.out.println("The Volume of Cuboid: "+volume+" meter");
+    }
+}
