ff5c5757242819a9312b96cb2dcaad9b901b396d
Lenovo <parvr7869@gmail.com>
2025-05-10 18:19:37 +0530

    bhaiya is this coorect

---
+++ src/not_cleared.java
@@ -0,0 +1,7 @@
+public class not_cleared {
+    public static void main(String[] args){
+        int marks = 30;
+        if (marks<33)
+            System.out.println("not cleared the exam "+marks);
+    }
+}
