a755406ea21a85fc01d55d0d989371f198a367f4
Lenovo <parvr7869@gmail.com>
2025-05-10 17:45:44 +0530

    assignment

---
+++ src/conditional_statement/driving_license.java
@@ -0,0 +1,13 @@
+package conditional_statement;
+
+public class driving_license {
+    public static void main(String[] args){
+        //wrtie a code to check whether person is elligible for driving licence based
+        // on condition of age, age should greater than 18
+        int age = 18;
+        if (age >= 18)
+            System.out.println("elligible for driving license >= "+age);
+
+
+        }
+}
