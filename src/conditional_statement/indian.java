fa9668f00e42c85eae20ecd394eb3fba3d9772d1
Lenovo <parvr7869@gmail.com>
2025-05-10 18:27:32 +0530

    india

---
+++ src/conditional_statement/indian.java
@@ -0,0 +1,11 @@
+package conditional_statement;
+
+public class indian {
+    public static void main(String[] args){
+        //if candidate is from INDIA then print INDIAN.
+        String citizenship = "INDIA";
+        if(citizenship == "INDIA")
+            System.out.println("candidate is from "+citizenship);
+
+    }
+}
--- src/conditional_statement/not_cleared.java
+++ src/conditional_statement/not_cleared.java
@@ -1,3 +1,5 @@
+package conditional_statement;
+
 public class not_cleared {
     public static void main(String[] args){
         int marks = 30;
