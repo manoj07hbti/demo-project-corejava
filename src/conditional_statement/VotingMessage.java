c245d0abd5c2b59c6878930166aafb7e07252ab9
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-11 15:50:31 +0530

    VotingEligibilty Code

---
+++ src/conditional_statement/VotingMessage.java
@@ -0,0 +1,17 @@
+package conditional_statement;
+
+public class VotingMessage {
+    public static void main(String[] args) {
+
+        //eligible then show message "Welcome to Voting System"
+        int age=17;
+        if (age>=18){
+            System.out.println("Welcome to Voting System");
+        }
+
+        if (age<18){
+            System.out.println("you are not elligible for Voting as your age is less than 18 Year");
+        }
+
+    }
+}
