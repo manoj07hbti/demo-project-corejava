a8385634471acedda77eec44d8e2b385ffcad06b
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-12 17:23:48 +0530

    Add GreaterThan & EqualToStatement

---
+++ src/conditional_statement/EqualToStatement.java
@@ -0,0 +1,16 @@
+package conditional_statement;
+
+public class EqualToStatement {
+
+    public static void main(String[] args) {
+
+        //Print "Hello This Statement is True" if x is equal to y.
+        int x=10;
+        int y=10;
+
+        if (x==y){
+            System.out.println("Hello This Statement is True");
+        }
+
+    }
+}
