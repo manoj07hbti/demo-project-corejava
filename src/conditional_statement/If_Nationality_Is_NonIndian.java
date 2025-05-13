d1094058995a269e2fb490b37e2c463dc475c72b
sunny  hans <sunnyhans8642@gmail.com>
2025-05-13 21:30:10 +0530

    condition_statement-assignment(updated)

--- src/conditional_statement/If_Nationality_Is_NonIndian.java
+++ src/conditional_statement/If_Nationality_Is_NonIndian.java
@@ -4,7 +4,8 @@ public class If_Nationality_Is_NonIndian {
     public static void main(String[] args) {
 
      String nation="pakistan";
-     if(nation!="india");
+     if(nation!="india");{
         System.out.println("Non indian");
+     }
     }
 }
--- src/conditional_statement/If_Stream.java
+++ src/conditional_statement/If_Stream.java
@@ -4,11 +4,14 @@ public class If_Stream {
     public static void main(String[] args) {
 
         String department="science";
-        if(department=="science");
+        if(department=="science") {
             System.out.println("science department ");
 
-            department="math";
-            if(department=="scincce");
+            department = "math";
+                if (department == "scincce"){
+                System.out.println("not science department");
 
+            }
+        }
     }
 }
