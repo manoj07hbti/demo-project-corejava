830bf1b107a2240f4cba74ec7ceec54c833159e4
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-05-13 13:31:00 +0530

    section

---
+++ src/conditionals/Section.java
@@ -0,0 +1,17 @@
+package conditionals;
+
+public class Section {
+
+    public static void main(String[] args) {
+
+        String name = "Tarun";
+        char section = 'A';
+        if (section == 'A') {
+            System.out.println(name+" belongs to section "+section);
+
+        }
+
+        }
+
+    }
+
