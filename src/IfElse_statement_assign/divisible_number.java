ce9290a1f5e72634a0d83948c8289119e2bc28d2
Lenovo <parvr7869@gmail.com>
2025-05-15 00:42:14 +0530

    changes

--- src/IfElse_statement_assign/divisible_number.java
+++ src/IfElse_statement_assign/divisible_number.java
@@ -5,11 +5,11 @@ public class divisible_number {
     //write a code to check any number which is  greater than 0  and divisible by 5 .
 
     public static void main(String[] args) {
-        int number = -85;
+        int number = 17;
         if(number > 0) {
             if(number%5==0){
                 System.out.println("number is divisible by 5... "+number);
-            }else{
+            }if(number%5!=0){
                 System.out.println("not divisible by 5 ...."+number);
             }
         }else{
