46d385ea587dcf95aec82fdfff73be5b01fb5dc9
Lenovo <parvr7869@gmail.com>
2025-05-14 22:43:37 +0530

    IfElse statement

---
+++ src/conditional_statement/IfElse_statement.java
@@ -0,0 +1,17 @@
+package conditional_statement;
+
+public class IfElse_statement {
+
+    //write a code to check voting elligiblity .
+    //if candidate is elligible then show mesage "Welcome to Voting System"
+    //if not then "you are not elligible for Voting as your age is less than 18 Year"
+
+    public static void main(String[] args) {
+        int age = 55;
+        if (age >= 18) {
+            System.out.println("Welcome to voting System " + age);
+        } else{
+                System.out.println("you are not elligible for Voting as your age is less than 18 Year " +age);
+            }
+        }
+    }
