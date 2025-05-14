7dd18a1cc937d9592267be143b17eb69e70ec800
Lenovo <parvr7869@gmail.com>
2025-05-15 00:29:02 +0530

    h

--- src/area.java
+++ src/area.java
@@ -3,7 +3,7 @@ public class area {
         // find the area of a circle with radius of 7.59
         float radius = 7.59f;
         float pi = 3.14f;
-        double area = (double)pi*radius**2;
+        double area = (double)pi*radius*radius;
         System.out.println("area of acircle is = "+area);
     }
 }
