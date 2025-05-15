aba0809921c2d8bed2b17d6e83aefe5547791311
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-15 20:27:37 +0530

    NestedIf Practice

---
+++ src/conditional_statement/NestedIf2.java
@@ -0,0 +1,26 @@
+package conditional_statement;
+
+public class NestedIf2 {
+    public static void main(String[] args) {
+        //Find the tallest student among 2 students
+        int n1 = 130, n2 = 150, n3 = 180;
+
+
+        if (n1 >= n2) {
+            if (n1 >= n3)
+                System.out.println("Student with height " + n1 + " is the tallest");
+            else {
+                System.out.println("Student with height " + n3 + " is the tallest");
+            }
+        } else {
+            if (n2 >= n3) {
+                System.out.println("Student with height " + n2 + " is the tallest");
+            } else {
+                System.out.println("Student with height " + n3 + " is the tallest");
+            }
+        }
+
+
+    }
+
+}
