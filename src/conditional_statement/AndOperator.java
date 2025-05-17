f00eebbbb8b4d125bd5643a875d2f234dee1122f
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-17 11:28:26 +0530

    And Operator

--- src/conditional_statement/AndOperator.java
+++ src/conditional_statement/AndOperator.java
@@ -9,6 +9,7 @@ public class AndOperator {
         int age =18;
         String city="Delhi";
 
+        // True && True = True Condition
         if (age>=18 &&  city=="Delhi"){
 
                 System.out.println("He is Eligible for Delhi Driving License..");
@@ -17,7 +18,47 @@ public class AndOperator {
                 System.out.println("He is not Eligible for Delhi Driving License..");
 
             }
+
+        // True && False = False Condition
+
+        city="Agra";
+        if (age>=18 &&  city=="Delhi"){
+
+            System.out.println("He is Eligible for Delhi Driving License..");
+        }
+        else {
+            System.out.println("He is not Eligible for Delhi Driving License..");
+
+        }
+
+        // False && True = False Condition
+
+        age=16;
+        if (age>=18 &&  city=="Delhi"){
+
+            System.out.println("He is Eligible for Delhi Driving License..");
+        }
+        else {
+            System.out.println("He is not Eligible for Delhi Driving License..");
+
+        }
+
+
+        // False && False = False Condition
+
+        city="Agra";
+        age= 17;
+        if (age>=18 &&  city=="Delhi"){
+
+            System.out.println("He is Eligible for Delhi Driving License..");
+        }
+        else {
+            System.out.println("He is not Eligible for Delhi Driving License..");
+
         }
 
     }
+
+
+}
 
