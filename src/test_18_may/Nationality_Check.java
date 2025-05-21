9f2a51a75684e9509edf5a5740397c9f2db5e6b2
Harshvardhanabc <hv1214293@gmail.com>
2025-05-21 14:24:29 +0530

    Test 18 May

--- src/test_18_may/Nationality_Check.java
+++ src/test_18_may/Nationality_Check.java
@@ -3,8 +3,8 @@ package test_18_may;
 public class Nationality_Check {
     // write a code to check person nationality , if nationality is India then check state should be Maharashtra then print both Nationality and state and if person is from different nation it should print that as well in else part.
     public static void main(String[] args) {
-        String Nationality = "USA";
-        String State = "California";
+        String Nationality = "INDIA";
+        String State = "DELHI";
         if (Nationality == "INDIA"){
             if (State == "MAHARASHTRA"){
                 System.out.println("Person is from "+State+" in "+Nationality);
