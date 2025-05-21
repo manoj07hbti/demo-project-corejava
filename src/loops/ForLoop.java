3a3a4c21349dc1a4578bc0ccdbfd2e37edf46e07
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-05-21 15:38:26 +0530

    Loops

---
+++ src/loops/ForLoop.java
@@ -0,0 +1,25 @@
+package loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+// print value 1 to 20 using increment
+
+        for (int i=1; i<=20; i++){
+            System.out.println(i);
+        }
+// print value 50 to 25 using decrement
+
+        for (int j=50; j>=25; j--){
+            System.out.println(j);
+        }
+// print value 25 to 50 using increment
+
+        for (int i=25; i<=50; i++){
+            System.out.println(i);
+        }
+
+    }
+}
+
+
---
+++ src/loops/WhileLoop.java
@@ -0,0 +1,31 @@
+package loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+// print value 1 to 20 using increment
+
+        int i=1;
+        while (i<=20){
+            System.out.println(i);
+            i++;
+        }
+
+// print value 50 to 25 using decrement
+
+        int j=50;
+        while (j>=25){
+            System.out.println(j);
+            j--;
+        }
+
+// print value 25 to 50 using increment
+
+    int k=25;
+    while (i<=50){
+        System.out.println(i);
+        i++;
+      }
+
+    }
+}
