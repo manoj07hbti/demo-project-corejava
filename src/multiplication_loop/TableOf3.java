75883ed83e4e179bbe4cdd07e711d566cade8dd7
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-05-21 17:25:50 +0530

    multiplication_Loops

---
+++ src/multiplication_loop/TableOf3.java
@@ -0,0 +1,14 @@
+package multiplication_loop;
+
+public class TableOf3 {
+    public static void main(String[] args) {
+
+        int number =3;
+
+        for (int i=1; i<=10; i++){
+            System.out.println(number +"*"+i+"="+number*i);
+        }
+
+
+    }
+}
---
+++ src/multiplication_loop/TableOf4.java
@@ -0,0 +1,13 @@
+package multiplication_loop;
+
+public class TableOf4 {
+    public static void main(String[] args) {
+
+        int number =4;
+
+        for (int i=1; i<=10; i++){
+            System.out.println(number +"*"+i+"="+ number*i);
+        }
+    }
+
+}
