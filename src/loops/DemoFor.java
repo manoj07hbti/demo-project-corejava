2d43e72185f328ad3e820639b759b55d1fa5204f
Neeraj Attri <neerajattri370@gmail.com>
2025-05-22 19:23:35 +0530

    loopday1

---
+++ src/loops/DemoFor.java
@@ -0,0 +1,30 @@
+package loops;
+
+public class DemoFor {
+
+    public static void main(String[] args) {
+
+
+        for (int i=0; i<=5; i++){
+
+            System.out.println("hey neeraj...."+ i);
+
+        }
+        for (int n=5; n>=0; n--){
+
+            System.out.println("decrement for loop...."+ n);
+        }
+
+
+        for (int v=1; v<=10; ){
+
+            int t=2;
+            System.out.println("printing table of 2 : "+ t*v);
+            v++;
+        }
+
+
+
+    }
+
+}
---
+++ src/loops/DemoWhile.java
@@ -0,0 +1,21 @@
+package loops;
+
+public class DemoWhile {
+
+    public static void main(String[] args) {
+
+        int n=1;
+        while (n<=5) {
+            System.out.println("while loop....." + n);
+            n++;
+        }
+
+        int v=1;
+        while (v>=5){
+
+            System.out.println("decrement of while loops..."+ v);
+            v--;
+        }
+
+    }
+}
