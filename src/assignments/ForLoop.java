255c55b2a770d344a2fd49ba04de1825b90243b2
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-05-24 19:07:29 +0530

    assignment..

---
+++ src/assignments/ForLoop.java
@@ -0,0 +1,19 @@
+package assignments;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+// print Even numbers using for loop
+
+        for (int i=2; i<=100; i+=2){
+            System.out.println("Even numbers= "+i);
+        }
+// print odd numbers using for loop
+
+        for (int i=1; i<=100; i+=2){
+            System.out.println("Odd numbers= "+i);
+        }
+    }
+
+}
---
+++ src/assignments/WhileLoop.java
@@ -0,0 +1,21 @@
+package assignments;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        int i=2;
+        while (i<=100){
+            System.out.println("print even numbers= "+i);
+            i+=2;
+        }
+
+
+        int j=1;
+        while (j<=100){
+            System.out.println("print odd number= "+j);
+            j+=2;
+        }
+
+    }
+}
