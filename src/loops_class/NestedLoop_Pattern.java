86fd0089819d2f4709d42fd3a5771ce2b4404f4f
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-24 20:41:59 +0530

    Star Pattern With Nested All loop

--- src/loops_class/NestedLoop_Pattern.java
+++ src/loops_class/NestedLoop_Pattern.java
@@ -10,5 +10,39 @@ public class NestedLoop_Pattern {
             }
             System.out.println();
         }
+
+
+        //Reverse Pattern
+        int row=6;
+        for (int k=row;k>=1;k--){
+            for (int l=1;l<=k;l++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Using Nested While Loop
+        int a=1;
+        while(a<=5){
+            int b=1;
+            while(b<=a){
+                System.out.print("* ");
+                b++;
+            }
+            System.out.println();
+            a++;
+        }
+
+        //Using Nested do while
+        int x=1;
+        do{
+            int y=1;
+            do{
+                System.out.print("* ");
+                y++;
+            }while(y<=x);
+            System.out.println();
+            x++;
+        }while(x<=5);
     }
 }
