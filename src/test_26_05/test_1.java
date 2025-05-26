64c2941f684d0102527ca3026cfaf07738ebf292
Lenovo <parvr7869@gmail.com>
2025-05-26 20:47:20 +0530

    test1

---
+++ src/test_26_05/test_1.java
@@ -0,0 +1,13 @@
+package test_26_05;
+
+public class test_1 {
+    public static void main(String[] args) {
+        //-3 -2 -1 0 1 2 3 print
+        int i;
+        for(i=-3; i<=3; i++)
+            System.out.println(+i);
+
+    }
+
+}
+
