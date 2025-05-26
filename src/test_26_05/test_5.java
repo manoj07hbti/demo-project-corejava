3988825e3dea73fb99a9aa4758078ad80eb0c873
Lenovo <parvr7869@gmail.com>
2025-05-26 21:21:59 +0530

    test 5

---
+++ src/test_26_05/test_5.java
@@ -0,0 +1,12 @@
+package test_26_05;
+
+public class test_5 {
+    public static void main(String[] args) {
+        int i = 3;
+        while (i <= 300) {
+            if (i % 2 == 0)
+                System.out.println(+i);
+            i++;
+        }
+    }
+}
