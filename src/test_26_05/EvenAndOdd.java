e840ac55de42a08a77d38258868d579c8f39c16d
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-05-27 01:09:39 +0530

    test_26_05

---
+++ src/test_26_05/EvenAndOdd.java
@@ -0,0 +1,17 @@
+package test_26_05;
+
+public class EvenAndOdd {
+    public static void main(String[] args) {
+
+// Write a code to check even and odd number 3 to 300
+
+        for (int i=3; i<=300; i++){
+            if (i%2==0){
+                System.out.println("print even numbers= "+i);
+            }else{
+                System.out.println("print odd numbers= "+i);
+            }
+        }
+    }
+
+}
---
+++ src/test_26_05/Program.java
@@ -0,0 +1,12 @@
+package test_26_05;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        for (int i=-3; i<=3; i++){
+            System.out.println(i);
+        }
+
+    }
+}
