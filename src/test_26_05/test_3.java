e3d203e4394ebcbdff90e41e2921c2ed794e563d
Lenovo <parvr7869@gmail.com>
2025-05-26 21:18:47 +0530

    test 3

---
+++ src/test_26_05/test_3.java
@@ -0,0 +1,17 @@
+package test_26_05;
+
+public class test_3 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        //
+        System.out.println("1");
+        System.out.println("12");
+        System.out.println("123");
+        System.out.println("1234");
+        System.out.println("12345");
+    }
+}
