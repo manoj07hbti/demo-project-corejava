d36fa7923c3df7073671e649fbd0b6bb7c1095df
Lenovo <parvr7869@gmail.com>
2025-05-26 12:32:19 +0530

    odd number using loops

--- src/loops/for_loop.java
+++ src/loops/for_loop.java
@@ -9,6 +9,25 @@ public class for_loop {
             System.out.println(+i );
         }
 
+
+    // while loops
+        int j=1;
+    while(j<=100){
+        if(j%2==1)
+            System.out.println(+j);
+        j++;
+    }
+
+    //do while loops
+        int k=1;
+    do{
+        if(k%2==1)
+            System.out.println(+k);
+        k++;
+    }while(k<=100);
+
+
+
         }
     }
 
