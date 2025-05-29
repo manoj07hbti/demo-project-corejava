946048348e02817b047240a75da929a2d80367fe
sunny  hans <sunnyhans8642@gmail.com>
2025-05-29 19:06:40 +0530

    Test26_05(Factorial)

--- src/test_26_05/Factorial_Q6.java
+++ src/test_26_05/Factorial_Q6.java
@@ -5,6 +5,25 @@ public class Factorial_Q6 {
 
         //Q6-write a code to make factorial of number example factorial of 4 will be 4*3*2*1=24
 
+          int number = 9 ;
+        int factorial = 1 ;
+        for( int i=number ; i>0; i-- ){
+            factorial*=i ;
+        } System.out.println("factorial of " +number + " is: " +factorial);
 
+
+         number = 12 ;
+         factorial = 1 ;
+        for (int i = 1; i <= number; i++) {
+            factorial*= i;
+        } System.out.println("factorial of " + number + " is: " + factorial);
+
+         number = 6 ;
+         factorial = 1 ;
+        int i=number ;
+        while( i>0 ){
+            factorial*=i ;
+            i--;
+        }  System.out.println("factorial of " + number + " is: " + factorial );
     }
  }
