a536d899b8d020d6805e95fe711b0b933c01ee5e
Lenovo <parvr7869@gmail.com>
2025-06-02 00:48:51 +0530

    54321

---
+++ src/loops/nested_loop1.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class nested_loop1 {
+    public static void main(String[] args) {
+        //54321
+        //4321
+        //321
+        //21
+        //1
+
+        for (int i = 5; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(+j);
+            }
+            System.out.println();
+
+        }
+
+    }
+}
