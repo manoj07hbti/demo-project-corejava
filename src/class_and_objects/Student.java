1dd137c076c085107495901c89b060e05e72efe9
Manoj Kumar <manojkumar.pcf@gmail.com>
2025-06-02 21:59:05 +0530

    Class Day1 - classes and objects

---
+++ src/class_and_objects/Student.java
@@ -0,0 +1,16 @@
+package class_and_objects;
+
+public class Student {
+
+    String name="Rahul";
+    int age=23;
+    String section="Science";
+
+
+    public static void main(String[] args) {
+
+        Student obj1= new Student();// default constructor
+        System.out.println(obj1.name+ " Age "+obj1.age+" Section "+obj1.section);
+
+    }
+}
