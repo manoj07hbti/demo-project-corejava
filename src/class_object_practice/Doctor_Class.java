5756e07240799d47716ee9fb1d14a830d7e24299
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-06-04 20:56:05 +0530

    Class adn Obj Practice Day 1

---
+++ src/class_object_practice/Doctor_Class.java
@@ -0,0 +1,37 @@
+package class_object_practice;
+
+public class Doctor_Class {
+
+    // properties
+
+    String name ="Amit Arora";
+    String specialization ="Orthopedic";
+    int yearsOfExperience=10;
+    String hospital="AIIMS";
+
+    public static void main(String[] args) {
+
+        Doctor_Class obj1= new Doctor_Class();
+        Doctor_Class obj2= new Doctor_Class();
+        Doctor_Class obj3= new Doctor_Class();
+        Doctor_Class obj4= new Doctor_Class();
+        Doctor_Class obj5= new Doctor_Class();
+
+        //To Print All Objects
+
+        System.out.println("Name : "+obj1.name);
+        System.out.println("Name : "+obj1.hospital);
+        System.out.println("Name : "+obj1.specialization);
+        System.out.println("Name : "+obj1.yearsOfExperience);
+        System.out.println("                                       ");
+        System.out.println("============================================");
+        System.out.println("============================================");
+
+        System.out.println("Name : "+obj2.name+" ; Hospital : "+obj2.hospital+" ; Specilist : "+obj2.specialization+" ; Experience : "+obj2.yearsOfExperience);
+        System.out.println("Name : "+obj3.name+" ; Hospital : "+obj3.hospital+" ; Specilist : "+obj3.specialization+" ; Experience : "+obj3.yearsOfExperience);
+        System.out.println("Name : "+obj4.name+" ; Hospital : "+obj4.hospital+" ; Specilist : "+obj4.specialization+" ; Experience : "+obj4.yearsOfExperience);
+        System.out.println("Name : "+obj5.name+" ; Hospital : "+obj5.hospital+" ; Specilist : "+obj5.specialization+" ; Experience : "+obj5.yearsOfExperience);
+
+    }
+
+}
---
+++ src/class_object_practice/Employee_Class.java
@@ -0,0 +1,27 @@
+package class_object_practice;
+
+public class Employee_Class {
+
+    String name="Rahul";
+    int empID=12;
+    String depart="Operations";
+
+    public static void main(String[] args) {
+
+
+
+        Employee_Class obj1= new Employee_Class();
+        Employee_Class obj2= new Employee_Class();
+        Employee_Class obj3= new Employee_Class();
+        Employee_Class obj4= new Employee_Class();
+        Employee_Class obj5= new Employee_Class();
+
+
+        System.out.println("Name : "+obj1.name +" ; Emp ID : "+obj1.empID+" ; Depart : "+obj1.depart );
+        System.out.println("Name : "+obj2.name +" ; Emp ID : "+obj2.empID+" ; Depart : "+obj2.depart );
+        System.out.println("Name : "+obj3.name +" ; Emp ID : "+obj3.empID+" ; Depart : "+obj3.depart );
+        System.out.println("Name : "+obj4.name +" ; Emp ID : "+obj3.empID+" ; Depart : "+obj4.depart );
+        System.out.println("Name : "+obj5.name +" ; Emp ID : "+obj3.empID+" ; Depart : "+obj5.depart );
+    }
+
+}
