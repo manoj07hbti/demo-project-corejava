c5e8495d07a21cceec0405cbcc3a63a52b50f027
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-06-09 20:04:46 +0530

    Function Practice and Test Question: Max_Element, Reverse_Array, and SecondLargest_Array

---
+++ src/test_8_june/Max_Element.java
@@ -0,0 +1,19 @@
+package test_8_june;
+
+public class Max_Element {
+
+    // Write a  code to find Maximum Element Given Array.
+
+    public static void main(String[] args) {
+
+        int element [] ={1,3,5,2,4};
+        int max= element[0];
+
+        for (int i=1;i<element.length;i++){
+            if (element[i]>max){
+                max=element[i];
+            }
+        }
+        System.out.println("The Maximum element in the Given Array..."+max);
+    }
+}
