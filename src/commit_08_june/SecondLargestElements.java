7a59a7dec7442d7ddb5b631be69c05ead457bf99
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-06-10 21:40:52 +0530

    commit_08_june

---
+++ src/commit_08_june/SecondLargestElements.java
@@ -0,0 +1,15 @@
+package commit_08_june;
+
+public class SecondLargestElements {
+
+    public static void main(String[] args) {
+
+        int array[]={12,35,1,10,34,1};
+
+        for (int number:array){
+            if (number==34){
+                System.out.println(number);
+            }
+        }
+    }
+}
