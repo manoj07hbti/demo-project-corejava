a18003cc1b5d0229bbec9340d402f9f89c8ccb93
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-06-11 22:57:14 +0530

    Input Parameter Practice

---
+++ src/class_object_method/method_input_parameter/Cube_Method_Parameter.java
@@ -0,0 +1,23 @@
+package class_object_method.method_input_parameter;
+
+public class Cube_Method_Parameter {
+
+    //- wrtie a method for cube of any given number
+
+    public void cube(int a){
+
+        int cube=a*a*a;
+
+        System.out.println("Cube of a number.."+cube);
+
+    }
+
+    public static void main(String[] args) {
+
+        Cube_Method_Parameter obj = new Cube_Method_Parameter();
+        obj.cube(32);
+        obj.cube(87);
+        obj.cube(54);
+        obj.cube(2);
+    }
+}
