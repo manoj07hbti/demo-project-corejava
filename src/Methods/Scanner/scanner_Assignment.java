b4a4c0b9ff2eec938779f72cbfe2ef59a3589c38
Harshvardhanabc <hv1214293@gmail.com>
2025-06-25 13:19:42 +0530

    scanner_Assignment

--- src/Methods/Scanner/scanner_Assignment.java
+++ src/Methods/Scanner/scanner_Assignment.java
@@ -4,26 +4,30 @@ import java.util.Scanner;
 
 public class scanner_Assignment {
 
+    //Addition
     public int Add(int a, int b){
         return a+b;
     }
+    //Subtraction
     public int Sub(int a, int b){
         return a-b;
     }
+    //Multiply
     public int Multi(int a, int b){
         return a*b;
     }
-    public double Divide(int a, int b){
+    //Divide
+    public double Divide(float a, float b){
         return a/b;
     }
-
+    //Input for first variable
     public int input_a(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter First Value: ");
         int a = scanner.nextInt();
         return a;
     }
-
+    //Input for Second variable
     public int input_b(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter First Value: ");
@@ -31,30 +35,33 @@ public class scanner_Assignment {
         return b;
     }
 
+    // Main Method
     public static void main(String[] args) {
 
         scanner_Assignment obj = new scanner_Assignment();
         Scanner scan = new Scanner(System.in);
+        String Ask = "Yes";
 
-        System.out.print("What You want to do. (1 for Addition, 2 for Subtraction, 3 for Multiply, 4 for Divide): ");
-        int Choice = scan.nextInt();
+        // While loop for Continuity
+        while (Ask.equals("Yes")){
 
-        if (Choice==1){
-            int result = obj.Add(obj.input_a(), obj.input_b());
-            System.out.println(result);
-        } else if (Choice==2) {
-            int result = obj.Sub(obj.input_a(),obj.input_b());
-            System.out.println(result);
-        } else if (Choice==3) {
-            int result = obj.Multi(obj.input_a(), obj.input_b());
-            System.out.println(result);
-        } else if (Choice==4) {
-            double result = obj.Divide(obj.input_a(), obj.input_b());
-            System.out.println(result);
-        }else {
-            System.out.println("Invalid Choice. Please Try Again.........");
+            System.out.print("What You want to do. (Addition, Subtraction, Multiply, Divide): ");
+            String Choice = scan.nextLine();
+
+            if (Choice.equals("Addition")){
+                System.out.println("Addition is "+obj.Add(obj.input_a(), obj.input_b()));
+            } else if (Choice.equals("Subtraction")) {
+                System.out.println("Subtraction is "+obj.Sub(obj.input_a(),obj.input_b()));
+            } else if (Choice.equals("Multiply")) {
+                System.out.println("Multiply is "+obj.Multi(obj.input_a(), obj.input_b()));
+            } else if (Choice.equals("Divide")) {
+                System.out.println("Divide is "+obj.Divide(obj.input_a(), obj.input_b()));
+            }else {
+                System.out.println("Invalid Choice. Please Try Again.........");
+            }
+
+            System.out.print("Do you Calculate again (Yes/No): ");
+            Ask = scan.nextLine();
         }
-
-
     }
 }
