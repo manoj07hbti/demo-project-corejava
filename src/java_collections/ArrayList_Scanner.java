6b04da4cdded37c57483e0e9f04fe5219b1d7f18
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-07-03 21:23:16 +0530

    Array_List_20_Element Collection Assignment

---
+++ src/java_collections/ArrayList_Scanner.java
@@ -0,0 +1,63 @@
+package java_collections;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayList_Scanner {
+
+    public ArrayList nameElement() {
+
+        ArrayList nameList = new ArrayList();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter 20 elements to add to the ArrayList:");
+
+        // Add 20 elements using Scanner
+        for (int i = 0; i<10; i++) {
+            System.out.print("Enter element #" + (i+1 ) + ": ");
+            String element = scanner.nextLine();
+            nameList.add(element);
+        }
+
+        return nameList;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList_Scanner obj =new ArrayList_Scanner();
+        ArrayList list =obj.nameElement();
+
+        //Using For Loop
+        for (int i=0; i<10;i++){
+            System.out.println("Using For loop Print element # :"+list.get(i));
+        }
+
+        System.out.println("\n");
+        System.out.println("***************************** While Loop *****************************");
+        // While Loop
+        int k=0;
+        while (k< 10){
+            System.out.println("Using for While Loop Print element #: "+k +" "+list.get(k));
+            k++;
+        }
+
+        System.out.println("\n");
+        System.out.println("***************************** Do while Loop *****************************");
+        // Using Do while
+        int a=0;
+        do {
+            System.out.println("Using for do While Loop Print element #: "+a +" "+list.get(a));
+            a++;
+        }while (a<10);
+
+        // Using advance for Loop
+
+        System.out.println("\n");
+        System.out.println("*****************************Advance For Loop*****************************");
+        // for (DataType var :collection/Array
+        for (Object storeName :list){
+            System.out.println("Using for Advance For Loop Print element #: "+storeName);
+        }
+    }
+}
---
+++ src/java_collections/Array_List_20_Element.java
@@ -0,0 +1,79 @@
+package java_collections;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Array_List_20_Element {
+
+    //Create one Arraylist object and add 20 elements
+       // 1. add
+        //2. get and print all using for loop , while , do while
+        //3. remove and print all using for loop , while , do while
+    //      Advanced loop
+
+    public ArrayList storeName(){
+
+        ArrayList list =new ArrayList();
+
+        list.add("Neeshu");
+        list.add("Rahul");
+        list.add("Suraj");
+        list.add("Sarvesh");
+        list.add("Vijo Mthew");
+        list.add("Shweta");
+        list.add("Abhishek");
+        list.add("Shaumya");
+        list.add("Varun");
+        list.add("Omika");
+        list.add("Jaideep");
+        list.add("Krishna");
+        list.add("Anjali");
+        list.add("Teena");
+        list.add("Khushboo");
+        list.add("Tarun");
+        list.add("Rakesh");
+        list.add("Sumit");
+        list.add("Rinky");
+        list.add("Hitesh");
+
+        return list;
+
+    }
+
+    public static void main(String[] args) {
+
+        //Create Object for Call Array list
+        Array_List_20_Element obj = new Array_List_20_Element();
+        ArrayList list = obj.storeName();
+        System.out.println("Printing Elements :"+list);
+
+        // USing Loop
+        for (int i=0;i< list.size();i++){
+            System.out.println("Using for Loop Print element #: "+i +" "+list.get(i));
+        }
+
+        // While Loop
+        int k=0;
+        while (k< list.size()){
+            System.out.println("Using for While Loop Print element #: "+k +" "+list.get(k));
+            k++;
+        }
+
+        // Using Do while
+        int a=0;
+        do {
+            System.out.println("Using for do While Loop Print element #: "+a +" "+list.get(a));
+            a++;
+        }while (a<list.size());
+
+        // Using advance for Loop
+
+        // for (DataType var :collection/Array
+        for (Object storeName :list){
+            System.out.println("Using for Advance For Loop Print element #: "+storeName);
+        }
+
+
+
+    }
+}
