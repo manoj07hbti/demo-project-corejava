2421dfd06852c3a4e5207df5416690a9ab8a4786
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-07-05 21:12:04 +0530

    Array_List_20_Element Collection Assignment

---
+++ src/java_collections/InputList.java
@@ -0,0 +1,53 @@
+package java_collections;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InputList {
+
+    //What is your name?
+    //Enter your favorite hobby:
+    //Type a city you want to visit:
+    //Enter a grocery item:
+    //Add a movie you like:
+    //Type a book you've read recently:
+    //Enter a friend's name:
+    //Add a skill you want to learn:
+    //Enter a programming language you know:
+    //Type a song you love:
+
+    public ArrayList userInputList(){
+        Scanner scanner =new Scanner(System.in);
+        ArrayList movies = new ArrayList();
+
+        String movie;
+        String skill;
+
+        System.out.println("Enter movie names you like (type 'stop' to finish):");
+
+        while (true) {
+            System.out.print("Add a movie you like: ");
+            movie = scanner.nextLine();
+
+            if (movie.equalsIgnoreCase("stop")) {
+                break;
+            }
+
+            movies.add(movie);
+        }
+
+        return movies;
+
+    }
+
+    public static void main(String[] args) {
+
+        InputList obj=new InputList();
+        ArrayList list=obj.userInputList();
+        System.out.println("\nYour favorite movies:");
+        String movies;
+        for (Object movie: list) {
+            System.out.println("- " + list);
+        }
+    }
+}
