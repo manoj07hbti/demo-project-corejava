eacf13fd074fd3893d88492415338396c96b47c3
Manoj Kumar <manojkumar.pcf@gmail.com>
2025-07-12 21:55:29 +0530

    Collections Day4- HashSet with Custom Object

--- src/java_collections/hashset/DemoHashSet.java
+++ src/java_collections/hashset/DemoHashSet.java
@@ -36,18 +36,43 @@ public class DemoHashSet {
 
     }
 
+    public void demoHashCode(){
+
+        String s1= "Hello";
+        System.out.println( " Hashcode of S1 is "+s1.hashCode());
+        String s2= "Hello";
+        System.out.println( " Hashcode of S2 is "+s2.hashCode());
+        String s3= "Java";
+        System.out.println( " Hashcode of S3 is "+s3.hashCode());
+        String s4="World";
+        System.out.println( " Hashcode of S4 is "+s4.hashCode());
+        String s5="World";
+        System.out.println( " Hashcode of S5 is "+s5.hashCode());
+
+        HashSet<String> message= new HashSet<>();
+        message.add(s1);
+        message.add(s2);
+        message.add(s3);
+        message.add(s4);
+        message.add(s5);
+
+        System.out.println("Printing HashSet "+message);
+
+    }
+
 
     public static void main(String[] args) {
 
         DemoHashSet obj= new DemoHashSet();
         HashSet<String> output=obj.createCourseData();
-        System.out.println(output);
+       // System.out.println(output);
 
+        obj.demoHashCode();
         System.out.println("Unique course with insertion order : "+obj.storeUniqueCourse());
 
-        for(String var : obj.storeUniqueCourse()){
+     /*   for(String var : obj.storeUniqueCourse()){
 
             System.out.println("Course name is :"+var);
-        }
+        }*/
     }
 }
