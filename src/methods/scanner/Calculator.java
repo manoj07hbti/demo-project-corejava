9fbf7be0c82b5aa3d373e5667b068ba8f4add1b2
sunny  hans <sunnyhans8642@gmail.com>
2025-07-14 21:20:33 +0530

    cal app

--- src/methods/scanner/Calculator.java
+++ src/methods/scanner/Calculator.java
@@ -21,33 +21,43 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-
+        String flag= "c";
         Scanner obj = new Scanner(System.in);
-        System.out.println("For percentage a= represent the number and " + "b represent the percentage you want");
-        System.out.println(" please enter value of a: ");
-        int a= obj.nextInt();
-        System.out.println("please enter value of b: ");
-        int b=obj.nextInt();
-        System.out.println("please enter your choice , 1-Add , 2-Subtract , 3-divide ," +
-                "4-multiply ,5-percentage");
+        while ( flag.equals("c") ){
 
-        int choice= obj.nextInt();
+            System.out.println("For percentage a= represent the number and " + "b represent the percentage you want");
+            System.out.println(" please enter value of a: ");
+            int a= obj.nextInt();
+            System.out.println("please enter value of b: ");
+            int b=obj.nextInt();
+            System.out.println("please enter your choice , 1-Add , 2-Subtract , 3-divide ," +
+                    "4-multiply ,5-percentage");
 
-        Calculator cal =  new Calculator();
-        if(choice== 1){
-            System.out.println("Addition of given number is: " +cal.add(a,b));
-        }
-        if(choice==2){
-            System.out.println("subtraction of given number is: " + cal.subtraction(a,b));
-        }
-        if(choice==3){
-            System.out.println("Division of given number  is: " +cal.divide(a,b));
-        }
-        if(choice==4){
-            System.out.println("multiplication of given number is: " +cal.multiply(a,b));
-        }
-        if(choice==5){
-            System.out.println(" percentage of given number is: " +cal.percentage(a,b));
+            int choice= obj.nextInt();
+
+            Calculator cal =  new Calculator();
+            if(choice== 1){
+                System.out.println("Addition of given number is: " +cal.add(a,b));
+            }
+            if(choice==2){
+                System.out.println("subtraction of given number is: " + cal.subtraction(a,b));
+            }
+            if(choice==3){
+                System.out.println("Division of given number  is: " +cal.divide(a,b));
+            }
+            if(choice==4){
+                System.out.println("multiplication of given number is: " +cal.multiply(a,b));
+            }
+            if(choice==5){
+                System.out.println(" percentage of given number is: " +cal.percentage(a,b));
+            }
+
+            Scanner obj1 = new Scanner(System.in);
+            System.out.println("press c to continue use and e to exit ");
+            flag=obj1.nextLine();
+            if (!flag.equals("c")){
+                break;
+            }
         }
     }
 }
--- src/test_29_june/Problems.java
+++ src/test_29_june/Problems.java
@@ -5,15 +5,13 @@ import java.util.Scanner;
 public class Problems {
 
      // Method to check and print if the number is divisible by 7 and not odd (even)
-        public static void checkNumber(int num) {
-            if (num >= 7 && num <= 190) {
+        public static void checkNumber() {
+            for ( int num = 7 ; num <=190 ; num++){
                 if (num % 7 == 0 && num % 2 == 0) {
                     System.out.println(num + " is divisible by 7 and is even.");
                 } else {
                     System.out.println(num + " does not satisfy both conditions (divisible by 7 and even).");
                 }
-            } else {
-                System.out.println("Number not in the valid range (7 to 190).");
             }
         }
 
@@ -30,14 +28,12 @@ public class Problems {
 
     //write a code to check any  number between 3 to 160 is divisible
     // by 9 and it should not be odd number
-    public static void numberCheck( int num){
-            if(num>=3 && num<=160){
+    public static void numberCheck(){
+        for ( int num = 3 ; num <=160 ; num++){
                 if(num%9 ==0 && num%2==0){
                     System.out.println(num + "number is divisible by 9 and not a odd number");
                 }else {
                     System.out.println(num + "either number is not divisible by 9 or an odd number");}
-            }else{
-                System.out.println("number is not in range");
             }
         }
 
@@ -54,19 +50,15 @@ public class Problems {
 
     //write a code to check any number number between 8 to 160 is divisible by
     // 11 and it should not be odd number
-    public static void number(int num){
-            if (num>=8 && num<=160){
+    public static void number(){
+        for ( int num = 8 ; num <=160 ; num++){
                 if (num%11==0 && num%2 == 0){
                     System.out.println(num + " number is divisible by 11 and not a odd number ");
                 }else {
                     System.out.println(num + "either number is not divisible by 11 or an odd number");
                 }
-            }else {
-                System.out.println("number is not in range");
-            }
+        }
     }
-
-
 
         public static void main(String[] args) {
             Scanner obj = new Scanner(System.in);
@@ -76,9 +68,7 @@ public class Problems {
             int choice= obj.nextInt();
 
             if(choice==1){
-                System.out.print("Enter a number between 7 and 190: ");
-                int userInput = obj.nextInt();
-                checkNumber(userInput);
+                checkNumber();
             }
             if(choice==2){
                 System.out.println(" enter a number between 1 to 10 to check sqaure of a number");
@@ -86,9 +76,7 @@ public class Problems {
                 squareNumber(userInput);
             }
             if (choice==3){
-                System.out.print("Enter a number between 3 and 160: ");
-                int userInput = obj.nextInt();
-                numberCheck(userInput);
+                numberCheck();
             }
             if (choice==4){
                 System.out.println(" enter a number between 1 to 10 to check cube of a number");
@@ -96,9 +84,7 @@ public class Problems {
                 cube(userInput);
             }
             if (choice==5){
-                System.out.print("Enter a number between 8 and 160: ");
-                int userInput = obj.nextInt();
-                number(userInput);
+                 number();
             }
         }
     }
