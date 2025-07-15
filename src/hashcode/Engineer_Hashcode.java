a1106f04d0f8ea7953839ed9d6b6b1879ce06467
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-07-16 00:22:15 +0530

    HashCode

---
+++ src/hashcode/Engineer_Hashcode.java
@@ -0,0 +1,41 @@
+package hashcode;
+
+import models.Engineer;
+
+import java.util.HashSet;
+
+public class Engineer_Hashcode {
+
+    public  void Engineer_Data(){
+
+        HashSet<Engineer> EngineerHashcode= new HashSet<>();
+
+        Engineer s1= new Engineer("Tarun",21,3 ,"CS");
+        System.out.println( " Hashcode of S1 is "+s1.hashCode());
+        Engineer s2= new Engineer("rahul",31,8 ,"IT");
+        System.out.println( " Hashcode of S2 is "+s2.hashCode());
+        Engineer s3= new Engineer("jai",23,1 ,"CS");
+        System.out.println( " Hashcode of S3 is "+s3.hashCode());
+        Engineer s4= new Engineer("prince",25,4 ,"IT");
+        System.out.println( " Hashcode of S4 is "+s4.hashCode());
+        Engineer s5= new Engineer("ankit",27,3 ,"CS");
+        System.out.println( " Hashcode of S5 is "+s5.hashCode());
+
+        EngineerHashcode.add(s1);
+        EngineerHashcode.add(s2);
+        EngineerHashcode.add(s3);
+        EngineerHashcode.add(s4);
+        EngineerHashcode.add(s5);
+
+        for (Engineer List: EngineerHashcode){
+        System.out.println("Name= " + List.getName() +" Age= "+ List.getAge() + " Experience= "+ List.getExperience() + " Department= "+List.getDepartment());
+        }
+   }
+
+    public static void main(String[] args) {
+
+        Engineer_Hashcode obj= new Engineer_Hashcode();
+        obj.Engineer_Data();
+    }
+}
+
---
+++ src/hashcode/Student_Hashcode.java
@@ -0,0 +1,43 @@
+package hashcode;
+
+import models.Student;
+
+import java.util.HashSet;
+
+public class Student_Hashcode {
+
+    public void Student_Data(){
+
+
+        HashSet<Student> StudentHashcode= new HashSet<>();
+
+        Student s1= new Student("tarun",21,30.45,"BCA");
+        System.out.println( " Hashcode of S1 is "+s1.hashCode());
+        Student s2= new Student("ayaan",23,56.80,"BTech");
+        System.out.println( " Hashcode of S2 is "+s2.hashCode());
+        Student s3= new Student("nirmal",30,45.50,"MCA");
+        System.out.println( " Hashcode of S3 is "+s3.hashCode());
+        Student s4= new Student("rohit",12,60.99,"BA");
+        System.out.println( " Hashcode of S4 is "+s4.hashCode());
+        Student s5= new Student("rahul",34,70.66,"MBA");
+        System.out.println( " Hashcode of S5 is "+s5.hashCode());
+
+
+        StudentHashcode.add(s1);
+        StudentHashcode.add(s2);
+        StudentHashcode.add(s3);
+        StudentHashcode.add(s4);
+        StudentHashcode.add(s5);
+
+        for (Student list: StudentHashcode) {
+            System.out.println("Name= " + list.getName() +" Roll No= "+ list.getRoll_No() + " Marks= "+ list.getMarks() + " Department= "+list.getDepartment());
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Student_Hashcode obj= new Student_Hashcode();
+        obj.Student_Data();
+    }
+}
