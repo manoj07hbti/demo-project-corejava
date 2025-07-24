af5992abcc5ecd76540f304e16b4b7d889e9de58
Neeraj Attri <neerajattri370@gmail.com>
2025-07-24 14:20:26 +0530

    Hashset

---
+++ src/java_collection/hashset/Demo.java
@@ -0,0 +1,76 @@
+package java_collection.hashset;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class Demo {
+
+    //write a code to store course name
+    public HashSet<String> createCourseData(){
+        //ClassName <DataType> objName= new ClassName<>();
+        HashSet<String> course= new HashSet<>();
+        course.add("Java");
+        course.add("Spring Boot");
+        course.add("Microservice");
+        course.add("Cloud");
+        course.add("Java");
+        course.add("Spring Boot");
+        course.add("Microservice");
+
+        return  course;
+    }
+
+    // we want to store unique data and also maintain insertion order
+    //LinkedHashSet
+
+    public LinkedHashSet<String> storeUniqueCourse(){
+        LinkedHashSet <String> course= new LinkedHashSet<>();
+        course.add("java");
+        course.add("Spring");
+        course.add("Spring Boot");
+        course.add("Microservices");
+
+        return course;
+
+    }
+
+    public void demoHashCode(){
+
+        String s1= "Hello";
+        System.out.println( " Hashcode of S1 is "+s1.hashCode());
+        String s2= "Hello";
+        System.out.println( " Hashcode of S2 is "+s2.hashCode());
+        String s3= "Java";
+        System.out.println( " Hashcode of S3 is "+s3.hashCode());
+        String s4="World";
+        System.out.println( " Hashcode of S4 is "+s4.hashCode());
+        String s5="World";
+        System.out.println( " Hashcode of S5 is "+s5.hashCode());
+
+        HashSet<String> message= new HashSet<>();
+        message.add(s1);
+        message.add(s2);
+        message.add(s3);
+        message.add(s4);
+        message.add(s5);
+
+        System.out.println("Printing HashSet "+message);
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Demo obj= new Demo();
+        HashSet<String> output=obj.createCourseData();
+        // System.out.println(output);
+
+        obj.demoHashCode();
+        System.out.println("Unique course with insertion order : "+obj.storeUniqueCourse());
+
+        for(String var : obj.storeUniqueCourse()){
+
+            System.out.println("Course name is :"+var);
+        }
+    }
+}
---
+++ src/java_collection/hashset/StudentHashset.java
@@ -0,0 +1,44 @@
+package java_collection.hashset;
+
+import model.Student;
+
+import java.util.LinkedHashSet;
+
+public class StudentHashset {
+
+    public void createStudentData(){
+
+        LinkedHashSet<Student> studentHashSet= new LinkedHashSet<>();
+
+        // now we need to create data for Student
+
+        Student s1= new Student("Raj",21,"CS");
+        System.out.println( " Hashcode of S1 is "+s1.hashCode());
+        Student s2= new Student("Rohit",22,"IT");
+        System.out.println( " Hashcode of S2 is "+s2.hashCode());
+        Student s3= new Student("Raj",21,"CS");
+        System.out.println( " Hashcode of S3 is "+s3.hashCode());
+        Student s4= new Student("Rohit",22,"IT");
+        System.out.println( " Hashcode of S4 is "+s4.hashCode());
+        Student s5= new Student("Mohit",23,"CS");
+        System.out.println( " Hashcode of S5 is "+s5.hashCode());
+
+        studentHashSet.add(s1);
+        studentHashSet.add(s2);
+        studentHashSet.add(s3);
+        studentHashSet.add(s4);
+        studentHashSet.add(s5);
+
+        for(Student var: studentHashSet){
+
+            System.out.println("Name: "+var.getName()+ " Age: "+var.getAge() +" Section: "+var.getSection());
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        StudentHashset obj= new StudentHashset();
+        obj.createStudentData();
+    }
+}
