dc781ee8756bee74f43d8895e5d65c020ba5155a
Neeraj Attri <neerajattri370@gmail.com>
2025-08-02 20:05:13 +0530

    oops -inheritance

---
+++ src/oops/inheritance/DemoA.java
@@ -0,0 +1,17 @@
+package oops.inheritance;
+
+public class DemoA {
+
+
+    public void m1(){
+        System.out.println("I am m1 method of Class DemoA");
+    }
+
+    public void m2(){
+        System.out.println("I am m2 method of Class DemoA");
+    }
+
+    public void m3(){
+        System.out.println("I am m3 method of Class DemoA");
+    }
+}
---
+++ src/oops/inheritance/Person.java
@@ -0,0 +1,8 @@
+package oops.inheritance;
+
+public class Person {
+
+    public void birth(){
+        System.out.println("This is birth method of person");
+    }
+}
