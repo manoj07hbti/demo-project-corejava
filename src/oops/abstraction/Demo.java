f620762f80d9c7bcf75cdd9d6715651ec8f9eec6
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-08-04 20:56:46 +0530

    Oops - Abstraction with Interface

---
+++ src/oops/abstraction/Demo.java
@@ -0,0 +1,10 @@
+package oops.abstraction;
+
+public abstract class Demo {
+
+    public abstract void m1();
+
+    public abstract void m2();
+    public abstract void m3();
+
+}
--- src/oops/abstraction/Dog_Interface_Abstraction.java
+++ src/oops/abstraction/Dog_Interface_Abstraction.java
@@ -1,6 +1,6 @@
 package oops.abstraction;
 
-public  class Dog_Interface_Abstraction implements Demo_Interface{
+public  class Dog_Interface_Abstraction extends Demo implements Demo_Interface{
 
 
     @Override
@@ -18,10 +18,30 @@ public  class Dog_Interface_Abstraction implements Demo_Interface{
         System.out.println("Dog is sleeping.");
     }
 
+    @Override
+    public void m1() {
+        System.out.println("This is M1 Method from Demo");
+    }
+
+    @Override
+    public void m2() {
+        System.out.println("This is M2 Method from Demo");
+    }
+
+    @Override
+    public void m3() {
+        System.out.println("This is M3 Method from Demo");
+    }
+
     public static void main(String[] args) {
         Dog_Interface_Abstraction obj=new Dog_Interface_Abstraction();
         obj.makeSound();
         obj.eat();
         obj.sleep();
+        obj.m1();
+        obj.m2();
+        obj.m3();
     }
+
+
 }
