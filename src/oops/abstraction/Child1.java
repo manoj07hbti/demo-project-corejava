db941831dd96b3e98aa27eaf296d21148d36c8ad
Neeraj Attri <neerajattri370@gmail.com>
2025-08-04 20:31:30 +0530

    oops -abstract

---
+++ src/oops/abstraction/Child1.java
@@ -0,0 +1,9 @@
+package oops.abstraction;
+
+public class Child1 extends DemoAbstraction{
+
+    @Override
+    public void m1() {
+        System.out.println("Child class");
+    }
+}
---
+++ src/oops/abstraction/TwoWheeler.java
@@ -0,0 +1,13 @@
+package oops.abstraction;
+
+public class TwoWheeler extends Vehicle{
+    @Override
+    public void repairEngine() {
+        System.out.println("Two wheeler Engine...");
+    }
+
+    @Override
+    public void service() {
+
+    }
+}
