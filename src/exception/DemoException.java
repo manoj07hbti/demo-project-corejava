0985547c4a7ff77cd817771026f9a9498a354a12
Manoj Kumar <manojkumar.pcf@gmail.com>
2025-08-18 21:57:16 +0530

    Day 40- Exception

--- src/exception/DemoException.java
+++ src/exception/DemoException.java
@@ -8,8 +8,18 @@ public class DemoException {
        int result= 0;
        try {
            result = a/b;
-       } catch (Exception e) {
+       }
+       catch (ArithmeticException e){
+           System.out.println("Exception...."+e);
+       }
+       catch (RuntimeException e) {
            System.out.println("Exception occurred "+e);;
+       }
+       catch (Exception e) {
+           System.out.println("Exception occurred "+e);;
+       }
+       finally {
+           System.out.println("This is finally block, it will always gets executed...");
        }
        System.out.println("after divide");
        return result;
@@ -20,9 +30,12 @@ public class DemoException {
        int []arr={2,4,5};
 
        try {
-           System.out.println( arr[3]);
+           System.out.println( arr[2]);
        } catch (Exception e) {
            System.out.println("Exception -----"+e);
+       }
+       finally {
+           System.out.println("This is finally block, it will always gets executed...");
        }
        System.out.println("after display...");
    }
@@ -31,7 +44,7 @@ public class DemoException {
     public static void main(String[] args) {
 
        DemoException obj = new DemoException();
-       System.out.println(obj.divide(6,0));
+       System.out.println(obj.divide(6,2));
        obj.display();
 
 
