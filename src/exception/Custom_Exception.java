d744c4e0fc063f384367ccfc14d8cfc4dd514289
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-08-20 21:14:50 +0530

    Exception : Day 2 Custom_Exception

---
+++ src/exception/Custom_Exception.java
@@ -0,0 +1,25 @@
+package exception;
+
+import java.time.temporal.ChronoUnit;
+
+public class Custom_Exception {
+
+    public void myMethod(int age) throws MyCustomException {
+        if (age<17){
+            throw new MyCustomException("Age must be 18 or above to vote.");
+        }else {
+            System.out.println("You are eligible to vote!");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Custom_Exception obj = new Custom_Exception();
+        try {
+            obj.myMethod(18);
+        } catch (MyCustomException e) {
+            System.out.println("Caught Exception: " + e.getMessage());;
+        }
+    }
+}
---
+++ src/exception/MyCustomException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class MyCustomException extends Exception {
+    public MyCustomException(String message) {
+        super(message); // pass message to Exception class
+    }
+}
